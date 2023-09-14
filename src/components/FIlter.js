37a369e86ba0c36803976affb8454fb3d4b8e6ca
Donovan Raber <donovan.raber@outlook.com>
2023-09-14 08:39:18 -0700

    completed assignment

---
+++ src/components/FIlter.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Item from "./Item";
+
+const Filter = ({handleCategoryChange}) => {
+
+    return (
+        <>
+        <div className="Filter">
+        <select name="filter" onChange={handleCategoryChange}>
+          <option value="All">Filter by category</option>
+          <option value="Produce">Produce</option>
+          <option value="Dairy">Dairy</option>
+          <option value="Dessert">Dessert</option>
+        </select>
+      </div>
+      </>
+    )
+}
+
+export default Filter
